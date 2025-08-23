4d7e82cc5f35c45a371f7b767bfcf39aaf746be5
fmt-codemonkey <fmt.codemonkey@gmail.com>
2025-08-23 03:05:15 -0400

    Majority Element

---
+++ tests/array_and_hashings/test_majority_element.py
@@ -0,0 +1,12 @@
+from neetcode_250.array_and_hashings.majority_element import Solution
+
+def test_examples():
+    s = Solution()
+    assert s.majorityElement([5, 5, 1, 1, 1, 5, 5]) == 5
+    assert s.majorityElement([2, 2, 2]) == 2
+
+def test_edge_cases():
+    s = Solution()
+    assert s.majorityElement([1]) == 1
+    assert s.majorityElement([3, 3, 4]) == 3
+    assert s.majorityElement([9, 1, 9, 2, 9, 3, 9]) == 9
